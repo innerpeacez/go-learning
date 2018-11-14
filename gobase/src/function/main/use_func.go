1baaf87b9ad6c6c5da0e447a3d50ede07539ea46
innerpeacez <zhaihw0417@163.com>
2018-11-14 17:39:57 +0800

    go function save args type

--- gobase/src/function/main/use_func.go
+++ gobase/src/function/main/use_func.go
@@ -8,6 +8,13 @@ func add(x int, y int) int {
 	return x + y
 }
 
+// 相同参数类型可以省略
+func sub(x, y int) int {
+	return x - y
+}
+
 func main() {
-	fmt.Print(add(10, 20))
+	fmt.Println(add(10, 20))
+
+	fmt.Println(sub(100, 20))
 }
