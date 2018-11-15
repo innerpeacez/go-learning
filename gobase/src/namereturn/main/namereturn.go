41cdefd2619d57778d52cd7b8706d7730f0cc901
innerpeacez <zhaihw0417@163.com>
2018-11-15 20:34:33 +0800

    name return

--- gobase/src/namereturn/main/namereturn.go
+++ gobase/src/namereturn/main/namereturn.go
@@ -1,7 +1,7 @@
 package main
 
 // 分割两位数
-func split(num int) (dig2, dig1 int) {
+func split(num int) ( /*十位数*/ dig2 /*个位数*/, dig1 int) {
 	dig1 = num * 4 / 9
 	dig2 = num - dig1
 	return dig2, dig1
