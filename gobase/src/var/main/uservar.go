36a7b005048e87a9a3123ace48d983e324d65071
innerpeacez <zhaihw0417@163.com>
2018-11-16 18:01:36 +0800

    var 定义变量，可以包级别或者函数级别

---
+++ gobase/src/var/main/uservar.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var java, shell, c bool
+var str string
+
+// bool类型默认值false，int类型默认值0,string类型默认值为""
+func main() {
+	var i int
+	fmt.Print(i, java, shell, c, str)
+}
