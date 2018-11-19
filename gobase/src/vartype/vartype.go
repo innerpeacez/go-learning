733d1a6f748dc49fbdd3ed3e7228ed09643e727f
innerpeacez <zhaihw0417@163.com>
2018-11-19 20:11:25 +0800

    go语言基本类型

---
+++ gobase/src/vartype/vartype.go
@@ -0,0 +1,25 @@
+package main
+
+// go basic types
+
+/**
+bool
+
+string
+
+int  int8  int16  int32  int64
+uint uint8 uint16 uint32 uint64 uintptr
+
+byte // uint8 的别名
+
+rune // int32 的别名
+	 // 代表一个Unicode码
+
+float32 float64
+
+complex64 complex128
+*/
+
+func main() {
+
+}
