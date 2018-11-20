2625afea30d6366e625302724415764762f4cdd4
innerpeacez <zhaihw0417@163.com>
2018-11-20 20:14:34 +0800

    for

--- gobase/src/constants/constants.go
+++ gobase/src/constants/constants.go
@@ -4,9 +4,14 @@ import "fmt"
 
 const PI = 3.141592653
 
+const (
+	Name = "innerpeacez"
+	Age  = 14
+)
+
 func main() {
-	const HELLO_WORLD = "你好，世界"
+	const HelloWorld = "你好,世界"
 	// PI = 1234,常量无法修改左值
 	fmt.Println(PI)
-	fmt.Println(HELLO_WORLD)
+	fmt.Println(HelloWorld)
 }
