792d83118abcfe47fd58b87672cba3575c77a729
innerpeacez <zhaihw0417@163.com>
2018-11-22 21:11:39 +0800

    定义数组

--- gobase/src/array/array.go
+++ gobase/src/array/array.go
@@ -9,4 +9,20 @@ func main() {
 	arr[0] = 20
 
 	fmt.Println(arr)
+
+	p := []int{2, 3, 5, 7, 9}
+
+	fmt.Println("p = ", p)
+	fmt.Println("p[1:4] = ", p[1:4])
+	// 省略下标代表从 0 开始
+	fmt.Println("p[:3] ==", p[:3])
+
+	// 省略上标代表到 len(s) 结束
+	fmt.Println("p[4:] ==", p[4:])
+
+	ints := make([]int, 5, 1)
+	fmt.Println(cap(ints))
+	i := ints[cap(ints)]
+	fmt.Println(i)
+
 }
