6786279704a205189b05fe13ecc16bfdcebd5ca1
innerpeacez <zhaihw0417@163.com>
2018-11-30 17:59:45 +0800

    go interface

---
+++ src/gobase/src/interface/interface.go
@@ -0,0 +1,5 @@
+package main
+
+type Interface interface {
+	printName(name string)
+}
---
+++ src/gobase/src/interface/print_name.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func printName(name string) {
+	fmt.Printf("name : %s", name)
+}
+
+func main() {
+	printName("innerpeacez")
+}
