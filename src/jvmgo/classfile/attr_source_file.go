8e173d854ebb188be78a6114caabe27018840478
innerpeacez <zhaihw0417@163.com>
2018-12-04 18:35:08 +0800

    SourceFile属性

---
+++ src/jvmgo/classfile/attr_source_file.go
@@ -0,0 +1,15 @@
+package classfile
+
+// 可选定长属性，用于指定源文件名
+type SourceFileAttribute struct {
+	cp              ConstantPool
+	sourceFileIndex uint16
+}
+
+func (self *SourceFileAttribute) readInfo(reader *ClassReader) {
+	self.sourceFileIndex = reader.readUint16()
+}
+
+func (self *SourceFileAttribute) FileName() string {
+	return self.cp.getUtf8(self.sourceFileIndex)
+}
