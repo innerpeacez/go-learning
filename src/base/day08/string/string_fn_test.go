89ffc2e003a88844e8f472d301b456cd36393b92
innerpeacez <zhaihw0417@163.com>
2019-04-13 20:37:15 +0800

    go string 常用工具包 strings,strconv

--- src/base/day08/string/string_fn_test.go
+++ src/base/day08/string/string_fn_test.go
@@ -1,6 +1,7 @@
 package string
 
 import (
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -8,10 +9,25 @@ import (
 func TestStringFn(t *testing.T) {
 
 	s := "A,B,C"
-	// 字符串的分割
+	// string 的分割
 	parts := strings.Split(s, ",")
 	for _, part := range parts {
 		t.Log(part)
 	}
+
+	// string 的连接
+	join := strings.Join(parts, "/")
+	t.Log(join)
+
+}
+
+func TestStr(t *testing.T) {
+	// 字符串的转换
+	s := "10"
+	if v, err := strconv.Atoi(s); err == nil {
+		t.Log(v + 10)
+	}
+	i := 10
+	t.Log(strconv.Itoa(i) + "str")
 
 }
