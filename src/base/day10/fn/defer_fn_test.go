f0ef2e49bd5cea374cd033f7c83f79ace89d531c
innerpeacez <zhaihw0417@163.com>
2019-04-16 20:23:50 +0800

    go defer 延长执行函数

--- src/base/day10/fn/defer_fn_test.go
+++ src/base/day10/fn/defer_fn_test.go
@@ -1,1 +1,26 @@
 package fn
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDefer(t *testing.T) {
+	// defer 函数
+	defer func() {
+		t.Log("Clear resources")
+	}()
+
+	t.Log("Started")
+	//panic("Fatal error") // 抛出异常
+}
+
+func Clear() {
+	fmt.Println("Clear resources")
+}
+
+func TestDefer2(t *testing.T) {
+	defer Clear() // panic 时defer函数也会执行
+	fmt.Println("Start")
+	//panic("error")
+}
