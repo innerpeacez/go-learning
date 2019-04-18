09368f5a81d43f5a8be52ce9f73124390dd172a0
innerpeacez <zhaihw0417@163.com>
2019-04-18 17:28:54 +0800

    go interface 定义及实现 , 自定义接口

--- src/base/day12/inteface/interface_test.go
+++ src/base/day12/inteface/interface_test.go
@@ -1,6 +1,9 @@
 package inteface
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // 定义一个接口
 type Programmer interface {
@@ -22,4 +25,30 @@ func TestInterface(t *testing.T) {
 	programmer := &GoProgrammer{}
 	hello := programmer.WriteHelloWorld()
 	t.Log(hello)
+}
+
+func hello() {
+	fmt.Println("hello")
+}
+
+// 自定义接口类型
+func Hello(hello func()) string {
+	hello()
+	return "hello"
+}
+
+type hello2 func()
+
+func (hello2 *hello2) hhello2() {
+	fmt.Println("hello 2")
+}
+
+func Hello2(hello2 hello2) string {
+	hello2()
+	return "hello"
+}
+
+func TestDefinition(t *testing.T) {
+	Hello(hello)
+
 }
